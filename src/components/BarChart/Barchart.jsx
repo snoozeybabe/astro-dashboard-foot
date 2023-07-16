d500a783ace21a9707e0ab3642d408d3fafbdc53
snoozeybabe <dz.kiki15@gmail.com>
2023-07-16 17:01:13 +0200

    feat(teams): adding real bar datas

--- src/components/BarChart/Barchart.jsx
+++ src/components/BarChart/Barchart.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { Data } from "../../utils/chartDatas.js";
 import { Bar } from "react-chartjs-2";
 
-const BarComponent = ({ chartData }) => {
+const BarComponent = ({ chartData, imgLogos }) => {
   return (
     <div className="relative w-[100%]  h-[90%] flex items-center flex-col m-0 p-0">
       <h2 className="text-center ">Team goals</h2>
@@ -25,13 +25,18 @@ const BarComponent = ({ chartData }) => {
           },
           responsive: true,
           maintainAspectRatio: false,
+
           plugins: {
             title: {
               display: false,
-              text: "Users gauned between 2022",
+              text: "All goals",
             },
             legend: {
               display: false,
+            },
+            labels: {
+              render: "image",
+              images: imgLogos,
             },
           },
         }}
@@ -42,20 +47,27 @@ const BarComponent = ({ chartData }) => {
 
 Chart.register(CategoryScale);
 
-export default function BarChart() {
+export default function BarChart({ datas }) {
+  const formattedImage = () => {
+    const images = datas.map((d) => {
+      return { width: 20, height: 20, src: d.logo };
+    });
+    console.log(images);
+    return images;
+  };
   const [chartData, setChartData] = useState({
-    labels: Data.map((data) => data.year),
+    labels: datas.map((data) => data.name),
     datasets: [
       {
-        label: "Users Gained ",
-        data: Data.map((data) => data.userGain),
+        label: "Goals",
+        data: datas.map((data) => data.goals),
         backgroundColor: ["#B0ADAA"],
       },
     ],
   });
   return (
     <div className="relative w-[100%]  h-[100%] m-0 p-0">
-      <BarComponent chartData={chartData} />
+      <BarComponent chartData={chartData} imgLogos={formattedImage(datas)} />
     </div>
   );
 }
